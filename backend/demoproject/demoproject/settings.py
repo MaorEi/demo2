7c1b3e915ff629b683a8ccce58c19805ed6b4c45
maore <maor.eitan.me@gmail.com>
2024-08-13 20:33:11 +0300

    docker-compose

--- backend/demoproject/demoproject/settings.py
+++ backend/demoproject/demoproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG').lower() == 'true'
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000',
-                        'http://' + os.getenv('CORS_ALLOWED_ORIGIN')]
+                        'http://' + os.getenv('CORS_ALLOWED_ORIGIN') + ':3000']
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.getenv('REACT_APP_ALLOWED_HOST')]
 
