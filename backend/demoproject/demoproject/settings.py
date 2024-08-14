e267c166e61ac0f06f20c02d83e0971b43ec2b71
maore <maor.eitan.me@gmail.com>
2024-08-14 23:31:47 +0300

    docker-compose

--- backend/demoproject/demoproject/settings.py
+++ backend/demoproject/demoproject/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.getenv('DEBUG').lower() == 'true'
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000',
                         'http://' + os.getenv('FRONTEND_IP') + ':3000']
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.getenv('BACKEND_IP')]
 
